89153df1ebb0684d78f1c32456baf91bdb9e71d6
SJayasri26 <sjayasri39@gmail.com>
2024-08-31 05:59:45 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,24 @@
+for (let i = 0; i < 5; i++) {
+    console.log("Hello World!");
+}
+
+//for loop
+let x;
+
+for (x = 2; x <= 4; x++) {
+    console.log("Value of x: " + x);
+}
+
+//while loop
+let val = 1;
+while (val<6){
+    console.log(val);
+    val +=1;
+}
+
+//do while
+let test=1;
+do{
+    console.log(test);
+    test++;
+}while(test<=5)
---
+++ object.js
@@ -0,0 +1,11 @@
+// JavaScript code demonstrating a simple object
+let school = {
+    name: 'Vivekananda School',
+    location: 'Delhi',
+    established: '1971',
+    displayInfo: function () {
+        console.log(`${school.name} was established 
+              in ${school.established} at ${school.location}`);
+    }
+}
+school.displayInfo();   
